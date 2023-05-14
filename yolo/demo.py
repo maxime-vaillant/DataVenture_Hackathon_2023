2df7a4d37db62d89953802c416127be12ceb36d1
Maxime Vaillant <max.vaillant1@orange.fr>
2023-05-14 09:43:28 +0200

    Add counter and label smoothing on demo.py

--- yolo/demo.py
+++ yolo/demo.py
@@ -4,10 +4,15 @@ import numpy as np
 from super_gradients.training import models
 from super_gradients.common.object_names import Models
 from streaming.client import RemoteCamera2 as RemoteCamera
-from matplotlib import pyplot as plt
 import cv2
 import numpy as np
 from ultralytics import YOLO
+
+
+def add_to_queue(queue, item):
+    if len(queue) >= 30:
+        queue.pop(0)
+    queue.append(item)
 
 
 def IoU(box1, box2) -> float:
@@ -42,6 +47,7 @@ def IoU(box1, box2) -> float:
     # return the intersection over union value
     return iou
 
+
 CLASSES_COLORS = {
     0: (255, 0, 0),
     1: (0, 255, 0),
@@ -50,19 +56,32 @@ CLASSES_COLORS = {
     4: (0, 255, 255),
 }
 
+CLASSES_NAME = {
+    0: "Broken",
+    1: "Gap",
+    2: "Good",
+    3: "Missing",
+    4: "No Cap"
+}
+
+COUNTER = {i: 0 for i in range(5)}
+
+
+def update_label(queue_label):
+    max_label = max(queue_label, key=queue_label.count)
+
+    return max_label
+
+
 def real_time_inference_v8():
-    model = YOLO('/Users/pierreadorni/Downloads/yolov8nMulti25.pt')
+    model = YOLO('../models/yolov8nMulti25.pt')
     rc = RemoteCamera("192.168.10.125", 9999)
     rc.connect()
 
     counter = 0
     objects = []
     while True:
-        t0 = time.time()
         img = rc.get_frame()
-        t1 = time.time()
-        print(f"Frame received in {t1 - t0} seconds")
-        # img = np.repeat(img[:, :, np.newaxis], 3, axis=2)
 
         if img is None:
             continue
@@ -72,20 +91,17 @@ def real_time_inference_v8():
         bboxes = res[0].boxes.xyxy.cpu().numpy().astype(int)
         confs = res[0].boxes.conf.cpu().numpy()
         labels = res[0].boxes.cls.cpu().numpy()
-        # label names
-        label_names = [model.names[i] for i in labels]
 
         toDelete = []
         # remove classes that overlap
-        for i, (x1, y1, x2, y2) in enumerate(bboxes):
-            for j, (x1_, y1_, x2_, y2_) in enumerate(bboxes):
-                if i != j and i not in toDelete and j not in toDelete and IoU([x1, y1, x2, y2], [x1_, y1_, x2_, y2_]) > 0.4:
+        for i, box in enumerate(bboxes):
+            for j, box_ in enumerate(bboxes):
+                if i != j and IoU(box, box_) > 0.8:
                     if confs[i] > confs[j]:
-                        # remove from lists
                         toDelete.append(j)
                     else:
-                        # remove from lists
                         toDelete.append(i)
+
         # delete all the boxes that overlap
         bboxes = np.delete(bboxes, toDelete, axis=0)
         confs = np.delete(confs, toDelete, axis=0)
@@ -93,37 +109,84 @@ def real_time_inference_v8():
 
         # plot the boxes
         res_plotted = img.copy()
+
         for (x1, y1, x2, y2), conf, label in zip(bboxes, confs, labels):
             cv2.rectangle(res_plotted, (x1, y1), (x2, y2), CLASSES_COLORS[label], 2)
-            cv2.putText(res_plotted, f"{model.names[label]} {conf:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, CLASSES_COLORS[label], 2)
+            cv2.putText(
+                res_plotted,
+                f"{model.names[label]} {conf:.2f}",
+                (x1, y1 - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.9,
+                CLASSES_COLORS[label],
+                2
+            )
 
         # for each bbox found
         objects_found_this_frame = set()
-        for x1, y1, x2, y2 in res[0].boxes.xyxy:
+        for bbox, label in zip(bboxes, labels):
             obj_found = False
             for obj in objects:
                 # if the object is already in the list
-                if IoU(obj["bbox"], [x1,y1,x2,y2]) > 0.4:
+                if IoU(obj["bbox"], bbox) > 0.4:
                     # update the bbox
-                    obj["bbox"] = [x1,y1,x2,y2]
+                    obj["bbox"] = bbox
                     obj_found = True
                     objects_found_this_frame.add(obj["counter"])
+                    add_to_queue(obj["label_queue"], label)
+                    obj["label"] = update_label(obj["label_queue"])
                     break
             # if the object is not in the list
             if not obj_found:
                 # add it
-                objects.append({"bbox": [x1,y1,x2,y2], "counter": counter})
+                objects.append({"bbox": bbox, "counter": counter, "label_queue": [label], "label": label})
                 objects_found_this_frame.add(counter)
                 counter += 1
 
         # remove objects that were not found in this frame
-        objects = [obj for obj in objects if obj["counter"] in objects_found_this_frame]
+        new_objects = []
 
-        # plot labels for each object
         for obj in objects:
-            x1, y1, x2, y2 = [int(o.item()) for o in obj["bbox"]]
-            print("obj", obj["counter"], "at", obj["bbox"])
-            cv2.putText(res_plotted, f"obj {obj['counter']}", (x1, y1 - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+            if obj["counter"] in objects_found_this_frame:
+                new_objects.append(obj)
+            else:
+                COUNTER[obj['label']] += 1
+
+        objects = new_objects
+
+        for obj, conf in zip(objects, confs):
+            x1, y1, x2, y2 = obj["bbox"]
+            label = obj["label"]
+            cv2.rectangle(res_plotted, (x1, y1), (x2, y2), CLASSES_COLORS[label], 2)
+            cv2.putText(
+                res_plotted,
+                f"{model.names[label]} {conf:.2f}",
+                (x1, y1 - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.9,
+                CLASSES_COLORS[label],
+                2
+            )
+
+            cv2.putText(
+                res_plotted,
+                f"obj {obj['counter']}",
+                (x1, y1 - 30),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.9,
+                (0, 255, 0),
+                2
+            )
+
+        cv2.putText(
+            res_plotted,
+            f"{COUNTER[0]} - {COUNTER[1]} - {COUNTER[2]} - {COUNTER[3]} - {COUNTER[4]}",
+            (30, 30),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.9,
+            (0, 255, 0),
+            2
+        )
 
         cv2.imshow("video stream", res_plotted)
 
@@ -152,7 +215,8 @@ def real_time_inference():
             x1, y1, x2, y2 = bbox.astype(int)
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             # draw the label
-            cv2.putText(img, f"{class_names[label]} {conf:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+            cv2.putText(img, f"{class_names[label]} {conf:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
+                        (0, 255, 0), 2)
         cv2.imshow("video stream", img)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
